9c868320e5c9fadc287276ebe52fb8e2dc9b3559
kobyvang <80074281+kobyvang@users.noreply.github.com>
2021-07-12 15:49:08 -0500

    Add files via upload

--- sports/pages/register/Register.jsx
+++ sports/pages/register/Register.jsx
@@ -1,1 +1,29 @@
+import "./register.css"
+
+export default function register() {
+    return (
+        <div className="register">
+            <div className="registerWrapper">
+                <div className="registerLeft">
+                    <h3 className="registerLogo">Ballerz</h3>
+                    <span className="registerDesc">Connect with people to enjoy some sports!
+                    </span>
+                </div>
+                <div className="registerRight">
+                    <div className="registerBox">
+                        <input placeholder="Email" className="registerInput" />
+                        <input placeholder="UserName" className="registerInput" />
+                        <input placeholder="Password" className="registerInput" />
+                        <input placeholder="Confirm(Password)" className="registerInput" />
+                        <button className="registerButton"> Sign up</button>
+            
+                        <button className="RegisterButton">Log into Account</button>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
 
