5aab1257cf26a265b1a672cdb26bc1300d691bc9
kobyvang <80074281+kobyvang@users.noreply.github.com>
2021-07-12 15:48:31 -0500

    Add files via upload

--- sports/pages/map/Map.jsx
+++ sports/pages/map/Map.jsx
@@ -1,1 +1,10 @@
+import "./map.css"
+
+export default function Map() {
+    return (
+        <iframe className="map"
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDZ2ONxTDXm8t4W0Fvomu3f8nXgtZW2QaM&q=Space+Needle,Seattle+WA">
+</iframe>
+    )
+}
 
