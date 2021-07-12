831ab0ccf34411e3017482fc75555ccf76f3bf38
kobyvang <80074281+kobyvang@users.noreply.github.com>
2021-07-12 15:48:49 -0500

    Add files via upload

--- sports/pages/profile/Profile.jsx
+++ sports/pages/profile/Profile.jsx
@@ -1,1 +1,35 @@
+import "./profile.css"
+import Topbar from "../../components/topbar/Topbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Feed from "../../components/feed/Feed"
+import Rightbar from "../../components/rightbar/Rightbar"
 
+
+function Profile() {
+    return (
+        <>
+        <Topbar />
+        <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+            <div className="profileRightTop">
+                <div className="profileCover">
+                <img src="/assets/final2.jpg" alt="" className="profileCoverImg"/>
+                <img src="/assets/jumpball.jpg" alt="" className="profileUserImg"/>
+                </div>
+                <div className="profileInfo">
+                    <h4 className="profileInfoName">Koby Vang</h4>
+                    <span className="profileInfoDesc">my description</span>
+                </div>
+                <div className="profileRightBottom">
+        <Feed />
+        <Rightbar />
+        </div>
+        </div>
+        </div>
+        </div>
+        </>
+    )
+}
+
+export default Profile
