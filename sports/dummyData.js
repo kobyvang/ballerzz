04e324f8a0a7dac226cd3a12d23536384b901d25
kobyvang <80074281+kobyvang@users.noreply.github.com>
2021-07-12 15:50:26 -0500

    Add files via upload

---
+++ sports/dummyData.js
@@ -0,0 +1,65 @@
+export const Users = [
+    {
+        id:1,
+        profilePicture: "/assets/running.jpg" ,
+        username: "Koby Vang"
+    },
+    {
+        id:2,
+        profilePicture: "/assets/coolguy.png",
+        username: "bob"
+    },
+    {
+        id:3,
+        profilePicture: "/assets/randomguy.png",
+        username:"john"
+    },
+    {
+        id:4,
+        profilePicture: "/assets/unnamed.jpg",
+        username:"ted"
+    }
+];
+
+export const Posts = [
+    {
+        id:1,
+        desc:"anyone wanna join at parkplace?",
+        photo:"/assets/jumpball.jpg",
+        date:"5 min ago",
+        userId:1,
+        like: 0,
+        going:0,
+        comments: 0,
+    },
+    {
+        id:2,
+        desc:"Hooping at willow!",
+        photo:"/assets/post/park.jpg",
+        date:"50 min ago",
+        userId:2,
+        like: 0,
+        going:0,
+        comments:0,
+    },
+    {
+        id:3,
+        desc:"league annoucement!",
+        photo:"/assets/league.jpg",
+        date:" 1 day ago",
+        userId:3,
+        like: 0,
+        going:0,
+        comments: 0,
+    },
+    {
+        id:4,
+        desc:"Football at clovernook!",
+        photo:"/assets/post/footballpark.jpg",
+        date:"5 days ago",
+        userId:4,
+        like:0,
+        going:0,
+        comments: 0,
+    }
+]
